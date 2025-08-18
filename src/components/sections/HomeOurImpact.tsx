2f260634a7485aff983ee4a8860c46320c50775b
shravan-more-testriq <shravanmore.testriq@gmail.com>
2025-08-18 12:52:45 +0530

    solved build errors

--- src/components/sections/HomeOurImpact.tsx
+++ src/components/sections/HomeOurImpact.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { FaArrowRight } from "react-icons/fa";
-import { caseStudiesData } from "@/app/lib/caseStudies";
 import Link from "next/link";
 
 const renderTitle = () => {
