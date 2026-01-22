48e5b66c884dc9d4e4a27b39ec4e0a550823bcd3
Prakash Mishra <mishraprakashcpn47@gmail.com>
2026-01-22 17:58:25 +0530

    modal

--- src/components/sections/WebappTestingHeroSection.tsx
+++ src/components/sections/WebappTestingHeroSection.tsx
@@ -132,7 +132,7 @@ const WebappTestingHeroSection: React.FC = () => {
         {/* Lightbox Modal */}
         <Lightbox
           isOpen={isLightboxOpen}
-          videoLink="https://youtu.be/wAPCSnAhhC8?si=6yNo43V82w91LiPt"
+          videoLink="https://yourvideolink.com"
           onClose={closeLightbox}
         />
       </div>
